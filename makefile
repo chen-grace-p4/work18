b60036e83ed05142b5dda8f85d0e5fa5db0bc829
chen-grace-p4 <gchen20@stuy.edu>
2021-12-08 21:44:58 -0500

    completed

--- makefile
+++ makefile
@@ -1,1 +1,9 @@
-making
+all: work18.o
+	gcc -o work18 work18.o
+work18.o: work18.c
+	gcc -c work18.c
+run:
+	./work18
+clean:
+	rm *.o
+	rm work18
--- work18.c
+++ work18.c
@@ -19,41 +19,53 @@ int main() {
 	//sending input to child
   int fds[2];
   pipe(fds);
-  printf("pipe created. fds[0]: %d fds[1]: %d\n", fds[0], fds[1]);
+
   //recieving response from child
   int fdstwo[2];
   pipe(fdstwo);
-  printf("pipe created. fdstwo[0]: %d fdstwo[1]: %d\n", fdstwo[0], fdstwo[1]);
 
   int f;
   f = fork();
 
+  //parent
   if (f) {
-    //parent
     close(fds[READ]);
-    //while (1) {
-    	printf("enter an input: ");
-   	char input[100];
-   	fgets(input, 100, stdin);
-    	write(fds[WRITE], input, strlen(input));
-    //}
-  
     close(fdstwo[WRITE]);
-    char line[100];
-    while (read(fdstwo[READ], line, sizeof(line)))
-      printf("response from child: [%s]\n", line);
+
+    while (1) {
+      printf("enter an input: ");
+      char input[100];
+      fgets(input, 100, stdin);
+      //writes input
+    	write(fds[WRITE], input, sizeof(input));
+
+      //reads response from child
+      char line[100];
+      read(fdstwo[READ], line, sizeof(line));
+      printf("response from child: %s\n", line);
+   }
   }
   else {
     //child
     close(fds[WRITE]);
-    char parentInput[100];
-    read(fds[READ], parentInput, sizeof(parentInput));
-    strcpy(parentInput, "manipulated");
-    
     close(fdstwo[READ]);
-    //sleep(2);
-    write(fdstwo[WRITE], parentInput, sizeof(parentInput));
+
+    while (1) {
+      char parentInput[100];
+      //reads input and modifies
+      read(fds[READ], parentInput, sizeof(parentInput));
+      //closes off
+      parentInput[strlen(parentInput)-1] = '\0';
+      //modifies
+      strcat(parentInput, "manipulated");
+      //closes off
+      parentInput[strlen(parentInput)] = '\0';
+
+      //writes input and sends back to parent
+      write(fdstwo[WRITE], parentInput, strlen(parentInput)+1);
+   }
+
   }
-  
+
   return 0;
 }
